8425b3f37cfb0f05951612d11fa57c0bdd8973e4
Rocío Zavala <66143482+rzavala1@users.noreply.github.com>
2023-07-11 15:12:42 -0600

    create project frontend
    create project frontend

---
+++ crypto_calculator_interface/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/@pages/Dashboard';
+
+const App = () => {
+  return (
+    <Router>
+    <Routes>
+      <Route exact path="/" element={<Dashboard />} />
+    </Routes>
+  </Router>
+  );
+};
+
+export default App;
---
+++ crypto_calculator_interface/src/components/@pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+function Dashboard() {
+  useEffect(() => {
+  }, []);
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
