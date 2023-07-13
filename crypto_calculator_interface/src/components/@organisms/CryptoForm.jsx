130c6c3f762cf05d570e3908d60c24abdccf302a
Rocío Zavala <66143482+rzavala1@users.noreply.github.com>
2023-07-13 03:55:41 -0600

    change url socket in env
    change url socket in env

--- crypto_calculator_interface/src/components/@organisms/CryptoForm.jsx
+++ crypto_calculator_interface/src/components/@organisms/CryptoForm.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import TableCrypto from "../@molecules/TableCrypto";
+const URl_SOCKET = process.env.REACT_APP_SOCKET_URL;
 
 function CryptoForm() {
 
   const [dataCrypto, setDataCrypto]=useState([])
   
   useEffect(() => {
-    const socket = io("http://localhost:4000"); 
+    const socket = io(URl_SOCKET); 
 
     socket.on("connect", () => {
       console.log("Connect socket server");
